4753a18c08b5200d28d2d57a5595dfd508275b7f
Margarida <9019412@student.zadkine.nl>
2024-10-11 21:51:07 +0200

    Add files via upload

---
+++ Opdracht_6/myscripts.js
@@ -0,0 +1,15 @@
+function calculate(num1, num2){
+    var calculate = num1+num2;
+    console.log(calculate) 
+}
+
+function multiplyByFive(num1){
+    var calculate = num1*5;
+    console.log(calculate)
+}
+
+function Calculateminutes(num1){
+    var calculate = num1/60;
+    console.log(calculate)
+}
+
