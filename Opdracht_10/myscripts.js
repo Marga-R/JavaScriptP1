8afcb9297914c16dde65fad371a843c34a2ac849
Margarida <9019412@student.zadkine.nl>
2024-11-06 11:31:02 +0100

    Add files via upload

--- Opdracht_10/myscripts.js
+++ Opdracht_10/myscripts.js
@@ -4,28 +4,52 @@ let num2 = document.getElementById("num2");
 
 
 function plus() {
-    let ans = Number(num1.value) + Number(num2.value);
 
-    answer.innerText = ans;
+    if(num1.value > 0 && num2.value > 0) {
+        let ans = Number(num1.value) + Number(num2.value);
+
+        answer.innerText = ans;
+    }
+    
+    else {
+        answer.innerText = "The number is too low"
+    }
 }
 
 function minus() {
-    let ans = num1.value - num2.value;
+    if(num1.value > 0 && num2.value > 0) {
+        let ans = Number(num1.value) - Number(num2.value);
 
-    answer.innerText = ans;
+        answer.innerText = ans;
+    }
+    
+    else {
+        answer.innerText = "The number is too low"
+    }
 }
 
 function divide() {
-    let ans = num1.value / num2.value;
+    if(num1.value > 0 && num2.value > 0) {
+        let ans = Number(num1.value) / Number(num2.value);
 
-    answer.innerText = ans;
+        answer.innerText = ans;
+    }
+    
+    else {
+        answer.innerText = "The number is too low"
+    }
     
 }
 
 function multiply() {
-    let ans = num1.value * num2.value;
+    if(num1.value > 0 && num2.value > 0) {
+        let ans = Number(num1.value) * Number(num2.value);
 
-    answer.innerText = ans;
+        answer.innerText = ans;
+    }
+    
+    else {
+        answer.innerText = "The number is too low"
+    }
 
 }
-
